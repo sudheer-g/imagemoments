0c84a6f24f09846226e2cdc03424794308641f50
sudheer-g <bharadwaj.sudheer@gmail.com>
2018-01-29 01:05:46 +0530

    Definitions written in a separate file

--- gui/basic_gui_layout.py
+++ gui/basic_gui_layout.py
@@ -9,6 +9,7 @@ from PIL import Image, ImageTk
 
 from moments import plot
 from config import result_values_file
+import definitions
 
 
 def open_file_explorer():
@@ -29,22 +30,19 @@ def main_window():
         master.minsize(500, 500)
         master.maxsize(500, 500)
         intro_label = Label(master,
-                            text="Identify any object in a Dataset via Image Recognition through Image Moments.\n\n",
+                            text=definitions.pokedex_intro,
                             font=12, wraplength=500)
         intro_label.pack()
         description_string = StringVar()
-        description_string.set(
-            "In image processing, computer vision and related fields, an image moment is a certain particular weighted average moment of the image pixels' intensities, or a function of such moments, usually chosen to have some attractive property or interpretation.Image moments are useful to describe objects after segmentation. Simple properties of the image which are found via image moments include area (or total intensity), its centroid, and information about its orientation.\n\n")
+        description_string.set(definitions.image_moment_definition)
         description_label = Label(master, textvariable=description_string, wraplength=500)
         description_label.pack()
         hu_moment_description = StringVar()
-        hu_moment_description.set(
-            "Hu Moments which are can be used to describe, characterize, and quantify the shape of an object in an image.Hu Moments are normally extracted from the silhouette or outline of an object in an image. By describing the silhouette or outline of an object, we are able to extract a shape feature vector (i.e. a list of numbers) to represent the shape of the object.We can then compare two feature vectors using a similarity metric or distance function to determine how similar the shapes are.\n\n")
+        hu_moment_description.set(definitions.hu_moment)
         hu_moments_label = Label(master, textvariable=hu_moment_description, wraplength=500)
         hu_moments_label.pack()
         zernike_moment_description = StringVar()
-        zernike_moment_description.set(
-            "Zernike moments are used to describe the shape of an object; however, since the Zernike polynomials are orthogonal to each other, there is no redundancy of information between the moments.")
+        zernike_moment_description.set(definitions.zernike_moment)
         zernike_moments_label = Label(master, textvariable=zernike_moment_description, wraplength=500)
         zernike_moments_label.pack()
         buttons_frame = Frame(master)
---
+++ gui/definitions.py
@@ -0,0 +1,23 @@
+__author__ = 'Sudheer'
+
+pokedex_intro = "Identify any object in a Dataset via Image Recognition through Image Moments.\n\n"
+
+image_moment_definition = "In image processing, computer vision and related fields, an image moment is a " \
+                          "certain particular weighted average moment of the image pixels' intensities, or a " \
+                          "function of such moments, usually chosen to have some attractive property or interpretation." \
+                          "Image moments are useful to describe objects after segmentation. Simple properties of" \
+                          " the image " \
+                          "which are found via image moments include area (or total intensity), its centroid," \
+                          " and information" \
+                          " about its orientation.\n\n"
+
+hu_moment = "Hu Moments which are can be used to describe, characterize, and quantify the shape " \
+                        "of an object in an image.Hu Moments are normally extracted from the silhouette or" \
+                        " outline of an object in an image. By describing the silhouette or outline of an" \
+                        " object, we are able to extract a shape feature vector (i.e. a list of numbers) to" \
+                        " represent the shape of the object.We can then compare two feature vectors using a" \
+                        " similarity metric or distance function to determine how similar the shapes are.\n\n"
+
+zernike_moment = "Zernike moments are used to describe the shape of an object; however, since the " \
+                        "Zernike polynomials are orthogonal to each other, there is no redundancy of information" \
+                        " between the moments."
